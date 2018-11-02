5b6eae8dcb34f9cbaffd06ba032a7766c816685e
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-02 14:20:56 -0700

    W2D5 - Implement hashes for Array, String, Hash classes

--- hash_map/lib/p02_hashing.rb
+++ hash_map/lib/p02_hashing.rb
@@ -4,11 +4,20 @@ end
 
 class Array
   def hash
+    return 777.hash if empty?
+    hash_num = 0
+
+    each_with_index do |el, idx|
+      hash_num ^= el % (idx + 1) ^ el * idx * size
+    end
+
+    hash_num.hash
   end
 end
 
 class String
   def hash
+    chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +25,14 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    array = to_a.map do |pair|
+      [self.string_to_num(pair[0]), self.string_to_num(pair[1])]
+    end.flatten.sort
+    array.hash
+  end
+
+  protected
+  def string_to_num(str)
+    str.to_s.chars.map(&:ord).reduce(:+)
   end
 end

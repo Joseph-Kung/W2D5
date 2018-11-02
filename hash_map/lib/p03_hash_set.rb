f36caa6096e9ac0585441759f63a88c38b46f8f7
Joseph-Kung <joseph.kung@hotmail.com>
2018-11-02 14:36:19 -0700

    W2D5 - Write all methods for HashSet class and pass all specs

--- hash_map/lib/p03_hash_set.rb
+++ hash_map/lib/p03_hash_set.rb
@@ -7,19 +7,30 @@ class HashSet
   end
 
   def insert(key)
+    self.count += 1
+
+    resize! if self.count > store.length
+
+    self[key] << key
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    return nil unless include?(key)
+
+    self.count -= 1
+    self[key].delete_at(self[key].find_index(key))
   end
 
   private
   attr_accessor :store
+  attr_writer :count
 
   def [](num)
-    store[num.hash % num_buckets] = num
+    store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -27,5 +38,15 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(store.length * 2) { [] }
+
+    store.each do |array|
+      array.each do |el|
+        new_store[el.hash % new_store.length] << el
+      end
+    end
+
+    store = new_store
+
   end
 end
